cf945832870e4c3a2562f5a96e7241bdb76a42b4
Andres <andruix93@gmail.com>
2025-09-24 19:14:52 -0600

    feat: integrates redis service

--- application/app.go
+++ application/app.go
@@ -4,15 +4,19 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 
 	return app
@@ -24,7 +28,12 @@ func (app *App) Start(ctx context.Context) error {
 		Handler: app.router,
 	}
 
-	err := server.ListenAndServe()
+	err := app.rdb.Ping(ctx).Err()
+	if err != nil {
+		return fmt.Errorf("failed to connect to redis: %w", err)
+	}
+
+	err = server.ListenAndServe()
 	if err != nil {
 		fmt.Printf("error starting server %w \n", err)
 	}
